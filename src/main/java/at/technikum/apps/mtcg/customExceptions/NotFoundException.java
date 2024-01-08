2ad1f58e07d5b793a5553ccd0929f5b16a937849
Helene Harrer <if22b036@technikum-wien.at>
2024-01-08 22:20:41 +0100

    added some new Exeptions and Injector modification

---
+++ src/main/java/at/technikum/apps/mtcg/customExceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package at.technikum.apps.mtcg.customExceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/at/technikum/apps/mtcg/customExceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package at.technikum.apps.mtcg.customExceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
